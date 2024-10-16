2b8a70e35c55f7d73e62b2d0be6b03ec36537fcd
Dylan Dorvilma <dda971@live.fr>
2024-10-16 16:28:25 +0200

    hotfix: deleted Layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,18 +1,18 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Header from "./components/Header";
 
-interface LayoutProps {
-  children: React.ReactNode;
-}
-export default function Layout({ children }: LayoutProps) {
-  return (
-    <>
-      <div>
-        <Header />
+// interface LayoutProps {
+//   children: React.ReactNode;
+// }
+// export default function Layout({ children }: LayoutProps) {
+//   return (
+//     <>
+//       <div>
+//         <Header />
 
-        <main>{children}</main>
-        <Footer />
-      </div>
-    </>
-  );
-}
+//         <main>{children}</main>
+//         <Footer />
+//       </div>
+//     </>
+//   );
+// }
